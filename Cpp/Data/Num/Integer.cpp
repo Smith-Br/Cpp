f621a3b9f0620827803e0283764c4440ed5541aa
Smith-Br <Entired.0@Gmail.com>
2022-10-06 22:34:48 +0700

    update

--- Cpp/Data/Num/Integer.cpp
+++ Cpp/Data/Num/Integer.cpp
@@ -1,1 +1,7 @@
 #include <iostream>
+using namespace std;
+
+int main() {
+    int a = 1;
+    cout << "bool:\t" << sizeof(bool) << " bytes" << endl;
+}
---
+++ Cpp/Out/Ex.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //1. Write a program that prints this sentence: "Hello, World!".
+    cout << "Hello, World!" << endl;
+
+    //2. Write a program that prints the sum of 12 and 15.
+    cout << 12 + 15 << endl;
+
+    //3. Sum of 1489 and 7348.
+    cout << 1489 + 7348 << endl;
+
+    //4. Sum of 324 + 553 = ?.
+    cout << "Sum of 324 + 553 = " << 324 + 553 << endl;
+
+    //5.
+    
+    return 0;
+}
