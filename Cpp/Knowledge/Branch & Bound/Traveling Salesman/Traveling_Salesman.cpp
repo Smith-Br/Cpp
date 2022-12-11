8b17579893e1b8049e4e2998e288e148ae011a0e
Mimi <Entired.0@Gmail.com>
2022-12-12 00:34:58 +0700

    Big UPDATES!!! :D

--- Cpp/Knowledge/Branch & Bound/Traveling Salesman/Traveling_Salesman.cpp
+++ Cpp/Knowledge/Branch & Bound/Traveling Salesman/Traveling_Salesman.cpp
@@ -1,10 +1,12 @@
 #include <fstream>
+#include <memory.h>
 
 using namespace std;
 
 int main() {
-   fstream inp("Traveling_Salesman.inp"); // input file.
-   fstream out("Traveling_Salesman.out"); // output file.
+    fstream inp("Traveling_Salesman.inp"); // input file.
+    fstream out("Traveling_Salesman.out"); // output file.
+
 
     return 0;
 }
